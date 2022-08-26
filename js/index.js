a6dfa15d3864e5f953fb4ef2e73820f39a066ff4
Jarry Yang <260902108@qq.com>
2022-08-26 19:27:37 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const glide = new Glide('.glide');
+const captionEl = document.querySelectorAll('.slide-caption')
+//监听轮播图挂载和轮播对应事件
+glide.on(['mount.after', 'run.after'], () => {
+    //获取正在轮播的caption
+    const caption = captionEl[glide.index]
+    anime({
+        targets: caption.children,
+        opacity: [0, 1],
+        //动画持续时间
+        duration: 400,
+        easing: 'linear',
+        //动画执行间隔400ms 起始等待300ms
+        delay: anime.stagger(400, 300),
+        //从下方移动到上方 []表示最大移动距离40 最后一个元素最大移动距离是10
+        translateY: [anime.stagger(40, 10), 0]
+        // strokeDashoffset: [anime.setDashoffset, 0],
+        // easing: 'easeInOutSine',
+        // duration: 1500,
+        // delay: function(el, i) { return i * 250 },
+        // direction: 'alternate',
+        // loop: true
+    })
+})
+//轮播之前重置透明度
+glide.on('run.before', () => {
+    document.querySelectorAll('.slide-caption>*').forEach(el => {
+        el.style.opacity = 0;
+    })
+})
+glide.mount()
---
+++ readme.md
@@ -0,0 +1,10 @@
+## 项目用到的一些库
+1.animejs 使用js控制html动画的库
+
+2.glidejs 控制轮播组件的库
+
+3.lsotope 成功案例的组件库 对案例进行排列筛选
+
+4.scrollreveal  控制页面滚动条滚动到某一元素时候出现的动画效果
+
+5.smooth-scroll 点击导航栏的时候流程滚动到对应区域
